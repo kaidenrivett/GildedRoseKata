1b5b4ddbc6766536def26ce27cdf766827acba7e
Kaiden <kaidenrivett@uvic.ca>
2020-09-27 10:30:57 -0700

    Committing Completed Lab 2

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,37 +9,73 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
-    @Disabled
+
+    // Exercise 4 / 5
+
     @Test
-    void test1() {
+    void daysPassedequalToSellin() {
         Item[] items = new Item[] { new Item("foo", 10, 9) };
         GildedRose app = new GildedRose(items);
+        app.updateQuality();
         nDaysPassed(app,10);
-        assertEquals(-1,app.items[0].quality);
+        assertEquals(0,app.items[0].quality);
     }
-
     @Test
-    void test2(){
-        Item[] items = new Item[] { new Item("foo1", 4, 10) };
+    void daysPassedgreaterThanSellin() {
+        Item[] items = new Item[] { new Item("foo1", 4, 10)};
         GildedRose app = new GildedRose(items);
-        nDaysPassed(app,6);
-        assertEquals(4,app.items[0].quality);
+        app.updateQuality();
+        nDaysPassed(app, 6);
+        assertEquals(0, app.items[0].quality);
     }
 
-    @Disabled
     @Test
-    void descriptive3(){
+    void uniqueItem(){
         Item[] items = new Item[] { new Item("Aged Brie", 10, 41) };
         GildedRose app = new GildedRose(items);
+        app.updateQuality();
         nDaysPassed(app,10);
-        assertEquals(51,app.items[0].quality);
+        assertEquals(50,app.items[0].quality);
     }
 
 
+    // Exercise1
+    @Test
+    void sellByDate() {
+        // once the sell by date has passed, quality degrades twice as fast
+        Item[] items = new Item[] { new Item("Apple", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        nDaysPassed(app,4);
+        assertEquals(42,app.items[0].quality);
 
-    private void nDaysPassed(GildedRose app,int days){
+    }
+
+    // Exercise2
+    @Test
+    void neverChangeSulfuras() {
+
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros",99999,80)};
+        GildedRose app = new GildedRose(items);
+        nDaysPassed(app, 999);
+        assertEquals(80, app.items[0].quality);
+        System.out.println(app.items[0].quality);
+
+    }
+
+    // Exercise3
+    @Test
+    void backstagePass() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert",13,29)};
+        GildedRose app = new GildedRose(items);
+        nDaysPassed(app, 12);
+        System.out.println("The quality value of Backstage Passes after 12 days have passed is: " + app.items[0].quality);
+    }
+
+
+    private void nDaysPassed(GildedRose app, int days){
         assert days >= 0;
-        for (int i=0;i<10;i++)
+        for (int i=0; i<days; i++)
             app.updateQuality();
     }
 }
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -10,15 +10,17 @@ public class TexttestFixture {
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 13, 29),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("foo1", 4, 10),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6) };
 
+
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 6;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
